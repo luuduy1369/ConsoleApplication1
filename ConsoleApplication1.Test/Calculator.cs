b954154ba812a8b0bf07016c92479ab02a69bd4e
luuduy1369 <khanhduyluu@gmail.com>
2015-10-27 23:41:07 +0700

    Third Commit

---
+++ ConsoleApplication1.Test/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
